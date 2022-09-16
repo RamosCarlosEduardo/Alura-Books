5dd8b549edf9d2b95517ba91fea83b487a42a607
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2022-09-16 02:19:44 -0300

    finishing mobile header

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-let menuIcon = document.querySelector(".cabecalho__menu-hamburguer")
-let menu = document.querySelector(".cabecalho__menu")
-
-menuIcon.addEventListener("click", () => {
-	menu.classList.toggle("cabecalho__menu--active")
-	menuIcon.classList.toggle("cabecalho__container__menu-icon--active")
-
-})
