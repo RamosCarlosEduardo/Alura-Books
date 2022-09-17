b75ca7efb00aea98f5d5faf487d27dd15215b461
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2022-09-17 00:38:45 -0300

    finishing mobile layout

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -2,17 +2,21 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    type: 'bullets',
   },
 
   // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
   // And if we need scrollbar
   // scrollbar: {
